4623bfb129ad970b5d1d0d62eb284d9470d8d960
TENCENT\p_xintlin <linxintao@shuame.com>
2019-01-18 11:09:43 +0800

    feat: finsh two sum solution

---
+++ 2.js
@@ -0,0 +1,48 @@
+/**
+ * @file Two Sum Solution
+ * @link https://leetcode.com/problems/two-sum/
+ */
+
+/**
+ * Brute force
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    let length = nums.length
+    for(let i=0;i<length;i++){
+      for(let j=i+1;j<length;j++){
+          if(nums[i]+nums[j]==target){
+           console.log(i,j)
+           return [i,j];
+          }
+      }
+    }
+    console.log('none')
+    return [];
+};
+
+
+/**
+ * Difference mapping table 
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+  var i,j,len = nums.length
+  var hash = {}
+  for(i=0;i<len;i++){
+    j = target - nums[i]
+    if(hash[nums[i]] !== undefined){
+      console.log(i,hash[nums[i]])
+      return [i,hash[nums[i]]]
+    }else{
+      hash[j] = i;
+    }
+  }
+};
+
+
+twoSum([2,7,11,15],9)
