7aed7112302f3de9b2073e280cec8079585bf055
Terry <244847258@qq.com>
2019-03-24 02:22:43 +0800

    Feat bst

--- structure/binarySearchTree.js
+++ structure/binarySearchTree.js
@@ -33,6 +33,14 @@ function BinarySearchTree(){
     }
   }
 
+  this.min = function () {
+    return minNode(root)
+  }
+  
+  this.max = function () {
+    return maxNode(root)
+  }
+
   this.inOrderTraverse = function(callback){
     inOrderTraverseNode(root,callback)
   }
@@ -44,7 +52,14 @@ function BinarySearchTree(){
   this.postOrderTraverse = function(callback){
     postOrderTraverseNode(root,callback)
   }
+
+  this.search = function (key) {
+    return searchNode(root,key)
+  }
   
+  this.remove = function (key) {
+    root = removeNode(root,key)
+  }
   let inOrderTraverseNode = function(node, callback){
     if(node !== null){
       inOrderTraverseNode(node.left,callback)
@@ -71,6 +86,73 @@ function BinarySearchTree(){
     }
   }
 
+  let minNode = function (node) {
+    if (node) {
+      while (node && node.left !== null) {
+        node = node.left
+      }
+      return node.key
+    }
+    return null
+  }
+
+  let maxNode = function (node) {
+    if (node) {
+      while (node && node.right !== null) {
+        node = node.right
+      }
+      return node.key
+    }
+    return null
+  }
+
+  let searchNode = function (node, key) {
+    if (node === null) {
+      return false
+    }
+    if (key < node.key) {
+      return searchNode(node.left,key)
+    } else if(key > node.key){
+      return searchNode(node.right,key)
+    }else{
+      return true
+    }
+  }
+
+  let removeNode = function (node, key) {
+    if (node === null) {
+      return null
+    }
+    if (key < node.key) {
+      node.left = removeNode(node.left,key)
+    } else if (key > node.key) {
+      node.right = removeNode(node.right,key)
+    } else {
+      if (node.left === null && node.right === null) {
+        node = null
+        return node
+      }
+      if (node.left === null) {
+        node = node.right
+        return node
+      }else if (node.right === null) {
+        node = node.left
+        return node
+      }
+
+      let aux = findMinNode(node.right)
+      node.key = aux.key
+      node.right = removeNode(node.right, aux.key)
+      return node
+    }
+  }
+
+  let findMinNode = function (node) {
+    while (node && node.left !== null) {
+      node = node.left
+    }
+    return node
+  }
 }
 
 //Test Code
@@ -90,6 +172,7 @@ tree.insert(14)
 tree.insert(18)
 tree.insert(25)
 
-tree.postOrderTraverse(function(key){
-  console.log(key)
-})
+// tree.postOrderTraverse(function(key){
+//   console.log(key)
+// })
+console.log(tree.search(24));
