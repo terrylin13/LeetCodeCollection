a5694b9b794cdbe4521c92122ff61f0c77222f76
Terry <terry13.lin@gmail.com>
2020-07-13 10:27:06 +0800

    Add 63 solution with go

---
+++ go/63.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	m := [][]int{{0, 0, 0}, {0, 1, 0}, {0, 0, 0}}
+	fmt.Println(uniquePathsWithObstacles(m))
+}
+func uniquePathsWithObstacles(obstacleGrid [][]int) int {
+	lengthX, lengthY := len(obstacleGrid), len(obstacleGrid[0])
+	dp := make([]int, lengthY)
+	if obstacleGrid[0][0] == 0 {
+		dp[0] = 1
+	}
+	for x := 0; x < lengthX; x++ {
+		for y := 0; y < lengthY; y++ {
+			if obstacleGrid[x][y] == 1 {
+				dp[y] = 0
+				continue
+			}
+			if y-1 >= 0 && obstacleGrid[x][y-1] == 0 {
+				dp[y] += dp[y-1]
+			}
+		}
+	}
+	return dp[len(dp)-1]
+}
