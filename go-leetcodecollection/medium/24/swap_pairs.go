166089dda62dbe85074ab0bd90a99e51d172062b
Terry13 <terry13.lin@gmail.com>
2022-04-28 18:32:20 +0800

    Update swap_pairs.go

--- go-leetcodecollection/medium/24/swap_pairs.go
+++ go-leetcodecollection/medium/24/swap_pairs.go
@@ -7,29 +7,14 @@ type ListNode struct {
 
 func swapPairs(head *ListNode) *ListNode {
 	dummyHead := &ListNode{0, head}
-	cur := dummyHead.Next
+	cur := dummyHead
 	for cur.Next != nil && cur.Next.Next != nil {
 		node1 := cur.Next
 		node2 := cur.Next.Next
-		cur.Next = node2.Next
+		cur.Next = node2
 		node1.Next = node2.Next
 		node2.Next = node1
 		cur = node1
-	}
-
-	return dummyHead.Next
-}
-
-func swapPairs2(head *ListNode) *ListNode {
-	dummyHead := &ListNode{0, head}
-	temp := dummyHead
-	for temp.Next != nil && temp.Next.Next != nil {
-		node1 := temp.Next
-		node2 := temp.Next.Next
-		temp.Next = node2
-		node1.Next = node2.Next
-		node2.Next = node1
-		temp = node1
 	}
 	return dummyHead.Next
 }
