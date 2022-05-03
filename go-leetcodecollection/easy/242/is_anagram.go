0a738e961c2342246902a79796343e03130d2b4a
Terry13 <terry13.lin@gmail.com>
2022-05-04 00:44:43 +0800

    add 242  solution

---
+++ go-leetcodecollection/easy/242/is_anagram.go
@@ -0,0 +1,40 @@
+package _242
+
+import "sort"
+
+// sorted
+func isAnagram(s, t string) bool {
+	s1, s2 := []byte(s), []byte(t)
+	sort.Slice(s1, func(i, j int) bool { return s1[i] < s1[j] })
+	sort.Slice(s2, func(i, j int) bool { return s2[i] < s2[j] })
+	return string(s1) == string(s2)
+}
+
+// mapping
+func isAnagram2(s, t string) bool {
+	var c1, c2 [26]int
+	for _, ch := range s {
+		c1[ch-'a']++
+	}
+	for _, ch := range t {
+		c2[ch-'a']++
+	}
+	return c1 == c2
+}
+
+func isAnagram3(s, t string) bool {
+	if len(s) != len(t) {
+		return false
+	}
+	cnt := map[rune]int{}
+	for _, ch := range s {
+		cnt[ch]++
+	}
+	for _, ch := range t {
+		cnt[ch]--
+		if cnt[ch] < 0 {
+			return false
+		}
+	}
+	return true
+}
---
+++ go-leetcodecollection/easy/242/is_anagram_test.go
@@ -0,0 +1,9 @@
+package _242
+
+import "testing"
+
+func TestIsAnagram(t *testing.T) {
+
+	t.Log(isAnagram("anagram", "nagaram"))
+
+}
