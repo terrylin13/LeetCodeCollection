a3f521302a8939434601e2fc215d3a4d58ee3456
Terry13 <terry13.lin@gmail.com>
2022-05-06 03:05:21 +0800

    Update validate_binary_search_tree.go

--- go-leetcodecollection/98/validate_binary_search_tree.go
+++ go-leetcodecollection/98/validate_binary_search_tree.go
@@ -1,5 +1,7 @@
 package validate_binary_search_tree
 
+import "math"
+
 /**
  * Definition for a binary tree node.
  * type TreeNode struct {
@@ -14,7 +16,40 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+// recursion
 func isValidBST(root *TreeNode) bool {
+	return helper(root, math.MinInt64, math.MaxInt64)
+}
 
-	return false
+func helper(root *TreeNode, lower, upper int) bool {
+	if root == nil {
+		return true
+	}
+
+	if root.Val <= lower || root.Val >= upper {
+		return false
+	}
+
+	return helper(root.Left, lower, root.Val) && helper(root.Right, root.Val, upper)
+}
+
+// inOrder
+func isValidBST2(root *TreeNode) bool {
+	stack := []*TreeNode{}
+	inOrder := math.MinInt64
+
+	for len(stack) > 0 || root != nil {
+		for root != nil {
+			stack = append(stack, root)
+			root = root.Left
+		}
+		root = stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+		if root.Val <= inOrder {
+			return false
+		}
+		inOrder = root.Val
+		root = root.Right
+	}
+	return true
 }
