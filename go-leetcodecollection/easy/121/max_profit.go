9fa9eb3f0dc847a69c6cd08249ff4e056fc80b05
Terry <terry13.lin@gmail.com>
2022-06-20 03:36:36 +0800

    Create max_profit.go

---
+++ go-leetcodecollection/easy/121/max_profit.go
@@ -0,0 +1,15 @@
+package main
+
+import "math"
+
+func maxProfit(prices []int) int {
+	min, ans := math.MaxInt, 0
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < min {
+			min = prices[i]
+		} else if prices[i]-min > ans {
+			ans = prices[i] - min
+		}
+	}
+	return ans
+}
