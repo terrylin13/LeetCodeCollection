e4ebde1fd50698d46de463766b07764eec43ae3c
Terry <terry13.lin@gmail.com>
2022-06-24 04:29:39 +0800

    Update max_profit.go

--- go-leetcodecollection/easy/121/max_profit.go
+++ go-leetcodecollection/easy/121/max_profit.go
@@ -13,3 +13,21 @@ func maxProfit(prices []int) int {
 	}
 	return ans
 }
+
+func maxProfit2(prices []int) int {
+	n := len(prices)
+	dp := make([][2]int, n)
+	dp[0][1] = -prices[0]
+	for i := 1; i < n; i++ {
+		dp[i][0] = max(dp[i-1][0], dp[i-1][1]+prices[i])
+		dp[i][1] = max(dp[i-1][1], -prices[i])
+	}
+	return dp[n-1][0]
+}
+
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
