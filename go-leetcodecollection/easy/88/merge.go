d933775477554a57fe2a06072763358b68018f2f
Terry <terry13.lin@gmail.com>
2022-06-27 21:59:03 +0800

    Create merge.go

---
+++ go-leetcodecollection/easy/88/merge.go
@@ -0,0 +1,51 @@
+package _88
+
+import "sort"
+
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	copy(nums1[m:], nums2)
+	sort.Ints(nums1)
+}
+
+func merge2(nums1 []int, m int, nums2 []int, n int) {
+	sorted := make([]int, 0, m+n)
+	p1, p2 := 0, 0
+	for {
+		if p1 == m {
+			sorted = append(sorted, nums2[p2:]...)
+			break
+		}
+		if p2 == n {
+			sorted = append(sorted, nums1[p1:]...)
+			break
+		}
+		if nums1[p1] < nums2[p2] {
+			sorted = append(sorted, nums1[p1])
+			p1++
+		} else {
+			sorted = append(sorted, nums2[p2])
+			p2++
+		}
+	}
+	copy(nums1, sorted)
+}
+
+func merge3(nums1 []int, m int, nums2 []int, n int) {
+	for p1, p2, tail := m-1, n-1, m+n-1; p1 >= 0 || p2 >= 0; tail-- {
+		var cur int
+		if p1 == -1 {
+			cur = nums2[p2]
+			p2--
+		} else if p2 == -1 {
+			cur = nums1[p1]
+			p1--
+		} else if nums1[p1] > nums2[p2] {
+			cur = nums1[p1]
+			p1--
+		} else {
+			cur = nums2[p2]
+			p2--
+		}
+		nums1[tail] = cur
+	}
+}
