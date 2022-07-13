0b736e2f83b7bb6e251aaa6b430d125e7c76d085
Terry <terry13.lin@gmail.com>
2022-07-13 12:42:45 +0800

    Create find_kth_largest.go

---
+++ go-leetcodecollection/medium/215/find_kth_largest.go
@@ -0,0 +1,77 @@
+package _215
+
+import (
+	"container/heap"
+	"math/rand"
+	"time"
+)
+
+//mini heap
+type IntHeap []int
+
+func (h IntHeap) Len() int { return len(h) }
+
+func (h IntHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+
+// func (h IntHeap) Top() int           { return h[0] }
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+func FindKthLargest(nums []int, k int) int {
+	h := &IntHeap{}
+	heap.Init(h)
+	for _, num := range nums {
+		if h.Len() < k {
+			heap.Push(h, num)
+		} else if (*h)[0] < num {
+			heap.Pop(h)
+			heap.Push(h, num)
+		}
+	}
+	return heap.Pop(h).(int)
+}
+
+// quickSelect
+func FindKthLargest2(nums []int, k int) int {
+	rand.Seed(time.Now().UnixNano())
+	return quickSelect(nums, 0, len(nums)-1, len(nums)-k)
+}
+func quickSelect(a []int, l, r, index int) int {
+	q := randomPartition(a, l, r)
+	if q == index {
+		return a[q]
+	} else if q < index {
+		return quickSelect(a, q+1, r, index)
+	}
+	return quickSelect(a, l, q-1, index)
+}
+
+func randomPartition(a []int, l, r int) int {
+	i := rand.Int()%(r-l+1) + l
+	a[i], a[r] = a[r], a[i]
+	return partition(a, l, r)
+}
+
+func partition(a []int, l, r int) int {
+	x := a[r]
+	i := l - 1
+	for j := l; j < r; j++ {
+		if a[j] <= x {
+			i++
+			a[i], a[j] = a[j], a[i]
+		}
+	}
+	a[i+1], a[r] = a[r], a[i+1]
+	return i + 1
+}
